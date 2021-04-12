ff82ba326704d0599b753a547be85dc6eed80c98
Humberto Marcone <hmarcone@gmail.com>
2021-04-12 18:17:54 -0300

    [Desenvolvimento] - adicionando update.bat

---
+++ update.bat
@@ -0,0 +1,11 @@
+echo off
+cls
+echo .
+echo ..........................................................................................
+echo Continue apenas depois de fazer o commit no repositorio local
+echo ..........................................................................................
+echo .
+pause
+git remote update --prune
+git rebase
+pause
